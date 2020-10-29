b08b35b99502eec95424b5f69b6d8b14aa175e16
AndyGeek <andy.eulogio@gmail.com>
2020-10-29 18:49:43 -0500

    Add lib files

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
 const { buildSchema } = require('graphql');
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const resolvers = require("./lib/resolvers");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const schema = buildSchema(`
-  type Query {
-    "Retorna un Hello"
-    hello: String,
-    "Retorna un saludo"
-    saludo: String
-  }
-`);
-
-const resolvers = {
-  hello: () => {
-    return 'Hola mundo';
-  },
-  saludo: () => {
-    return 'Hola a todos';
-  },
-};
+const schema = buildSchema(
+  readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+);
 
 app.use(
   '/api',
